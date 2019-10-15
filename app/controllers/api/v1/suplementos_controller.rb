007c05cdb8bf4a95f9376f800dbae7927473d042
Bruno Tostes <btfjulio@hotmail.com>
2019-10-15 18:01:26 -0300

    add number of pages to json

--- app/controllers/api/v1/suplementos_controller.rb
+++ app/controllers/api/v1/suplementos_controller.rb
@@ -1,20 +1,18 @@
 class Api::V1::SuplementosController < Api::V1::BaseController
-  before_action :set_store, only: :index
   acts_as_token_authentication_handler_for User
 
     def index
       if params[:query].present?
-          @suplementos = Suplemento.order(:price_cents).sup_search(params[:query]).page(params[:page]).per(50)
+          query_result = Suplemento.order(:price_cents).sup_search(params[:query])
+          @suplementos = query_result.page(params[:page]).per(50)
+          total_pages = (query_result / 50).to_i
       else
           @suplementos = Suplemento.all.page(params[:page]).per(50)
+          total_pages = (Suplemento.count / 50).to_i
       end
-      @headers = [{total_pages: @suplementos.count/5 }]
+      @headers = [{
+        total_pages: total_pages
+        }]
     end 
-
-    private
-
-    def set_store
-      @store = Store.find(params[:store_id])
-    end
 
 end
