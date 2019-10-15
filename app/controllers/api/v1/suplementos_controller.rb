86642a341acc4d5cd2bcf39948cbfe8171f93872
Bruno Tostes <btfjulio@hotmail.com>
2019-10-15 18:11:43 -0300

    add number of pages to json

--- app/controllers/api/v1/suplementos_controller.rb
+++ app/controllers/api/v1/suplementos_controller.rb
@@ -5,10 +5,10 @@ class Api::V1::SuplementosController < Api::V1::BaseController
       if params[:query].present?
           query_result = Suplemento.order(:price_cents).sup_search(params[:query])
           @suplementos = query_result.page(params[:page]).per(50)
-          total_pages = (query_result / 50).to_i
+          total_pages = (query_result.to_f / 50).ceil
       else
           @suplementos = Suplemento.all.page(params[:page]).per(50)
-          total_pages = (Suplemento.count / 50).to_i
+          total_pages = (Suplemento.count.to_f / 50).ceil
       end
       @headers = [{
         total_pages: total_pages
