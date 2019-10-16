27405eaf712b825881cd7fe4adf595ffda716bd6
Bruno Tostes <btfjulio@hotmail.com>
2019-10-16 14:43:20 -0300

    add price filter to api

--- app/controllers/api/v1/suplementos_controller.rb
+++ app/controllers/api/v1/suplementos_controller.rb
@@ -1,14 +1,19 @@
 class Api::V1::SuplementosController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User
 
-    def index
+    def index 
+      if params[:changed].present?
+        @suplementos = Suplemento.where(price_changed: true)
+      else
+        @suplementos = Suplemento.all
+      end
       if params[:query].present?
-          query_result = Suplemento.order(:price_cents).sup_search(params[:query])
+          query_result = @suplementos.order(:price_cents).sup_search(params[:query])
           @suplementos = query_result.page(params[:page]).per(50)
           total_pages = (query_result.size.to_f / 50).ceil
       else
-          @suplementos = Suplemento.all.page(params[:page]).per(50)
-          total_pages = (Suplemento.count.to_f / 50).ceil
+          @suplementos = @suplementos.page(params[:page]).per(50)
+          total_pages = (@suplementos.count.to_f / 50).ceil
       end
       @headers = [{
         total_pages: total_pages
