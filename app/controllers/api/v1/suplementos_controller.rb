98074e903e81dff3e22b8d07de4cd3932bb1d54a
Bruno Tostes <btfjulio@hotmail.com>
2019-11-07 17:09:51 -0300

    add filter to index

--- app/controllers/api/v1/suplementos_controller.rb
+++ app/controllers/api/v1/suplementos_controller.rb
@@ -2,19 +2,13 @@ class Api::V1::SuplementosController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User
 
     def index 
-      if params[:changed].present?
-        @suplementos = Suplemento.where(price_changed: true)
-      else
-        @suplementos = Suplemento.all
-      end
-      if params[:query].present?
-          query_result = @suplementos.order(:price_cents).sup_search(params[:query])
-          @suplementos = query_result.page(params[:page]).per(50)
-          total_pages = (query_result.size.to_f / 50).ceil
-      else
-          @suplementos = @suplementos.page(params[:page]).per(50)
-          total_pages = (@suplementos.count.to_f / 50).ceil
-      end
+      @suplementos = Suplemento.all
+      @suplementos = Suplemento.where(price_changed: true) if params[:changed].present?
+      @suplementos = @suplementos.order(:price_cents).store_search(params[:store]) if params[:store].present?
+      @suplementos = @suplementos.order(:price_cents).seller_search(params[:seller]) if params[:seller].present?
+      @suplementos = @suplementos.order(:price_cents).name_search(params[:name]) if params[:name].present? 
+      total_pages = (@suplementos.count.to_f / 50).ceil
+      @suplementos = @suplementos.page(params[:page]).per(50)
       @headers = [{
         total_pages: total_pages
         }]
