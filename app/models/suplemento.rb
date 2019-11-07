35609b5e09444e90acb5d3a5aedca517e5d48b1e
Bruno Tostes <btfjulio@hotmail.com>
2019-11-07 17:10:37 -0300

    add filter to index

--- app/models/suplemento.rb
+++ app/models/suplemento.rb
@@ -2,8 +2,20 @@ class Suplemento < ApplicationRecord
   belongs_to :store
   monetize :price_cents
   include PgSearch::Model
-  pg_search_scope :sup_search,
-  against: [:name, :seller, :brand, :promo],
+  pg_search_scope :name_search,
+  against: :name,
+  using: {
+    tsearch: { prefix: true }
+  }
+  pg_search_scope :seller_search,  
+  against: :seller,
+  using: {
+    tsearch: { prefix: true }
+  }
+  pg_search_scope :store_search,  
+  associated_against:  { 
+    store: :name 
+    } ,
   using: {
     tsearch: { prefix: true }
   }
