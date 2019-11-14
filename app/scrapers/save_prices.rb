2ccda42ba2a5ef775f96dfc9840d6d226786cf7a
Bruno Tostes <btfjulio@hotmail.com>
2019-11-14 16:10:24 -0300

    add average filter

--- app/scrapers/save_prices.rb
+++ app/scrapers/save_prices.rb
@@ -6,9 +6,10 @@ class SavePrices
             p.price = sup.price_cents
             p.save
             sup.average = sup.prices.average(:price).to_i
-            sup.diff = (sup.price_cents - sup.average)/sup.price_cents
+            sup.diff = (sup.price_cents - (sup.average || 0))/ (sup.price_cents != 0 ? sup.price_cents/100 : 1)
             sup.save
             p p
         end
     end
 end
+
