16993132143d02c4c15adbbe1c0971976b6d42c7
Bruno Tostes <btfjulio@hotmail.com>
2019-11-14 15:17:02 -0300

    add average filter

--- app/controllers/api/v1/suplementos_controller.rb
+++ app/controllers/api/v1/suplementos_controller.rb
@@ -2,13 +2,14 @@ class Api::V1::SuplementosController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User
 
     def index 
-      @suplementos = Suplemento.all.order(:price_cents)
+      @suplementos = Suplemento.all
       @suplementos = @suplementos.where('diff < 0').order(:diff) if params[:average].present?
       @suplementos = @suplementos.where(price_changed: true) if params[:changed].present?
       @suplementos = @suplementos.where(supershipping: true) if params[:supershipping].present?
       @suplementos = @suplementos.store_search(params[:store]) if params[:store].present?
       @suplementos = @suplementos.seller_search(params[:seller]) if params[:seller].present?
-      @suplementos = @suplementos.name_search(params[:name]) if params[:name].present? 
+      @suplementos = @suplementos.name_search(params[:name]) if params[:name].present?
+      @suplementos = @suplemento.order(:price_cents) 
       total_pages = (@suplementos.count.to_f / 50).ceil
       @suplementos = @suplementos.page(params[:page]).per(50)
       @headers = [{
