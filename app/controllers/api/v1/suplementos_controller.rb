e07d95a0b46ce43ad1add2ef29dff04759008b89
Bruno Tostes <btfjulio@hotmail.com>
2019-11-19 15:09:13 -0300

    change order api

--- app/controllers/api/v1/suplementos_controller.rb
+++ app/controllers/api/v1/suplementos_controller.rb
@@ -3,13 +3,13 @@ class Api::V1::SuplementosController < Api::V1::BaseController
 
     def index 
       @suplementos = Suplemento.all
+      @suplementos = @suplementos.order(:price_cents) unless params[:average] == true
       @suplementos = @suplementos.where('price_cents < average').order('price_cents - average') if params[:average] == true
       @suplementos = @suplementos.where(price_changed: true) if params[:changed].present?
       @suplementos = @suplementos.where(supershipping: true) if params[:supershipping].present?
       @suplementos = @suplementos.store_search(params[:store]) if params[:store].present?
       @suplementos = @suplementos.seller_search(params[:seller]) if params[:seller].present?
       @suplementos = @suplementos.name_search(params[:name]) if params[:name].present?
-      @suplementos = @suplementos.order(:price_cents)
       total_pages = (@suplementos.count.to_f / 50).ceil
       @suplementos = @suplementos.page(params[:page]).per(50)
       @headers = [{
