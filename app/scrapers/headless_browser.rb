fa6d474ee7bac350407879c0c95c893e6e2fb6b8
Bruno Tostes <btfjulio@hotmail.com>
2019-11-28 17:45:46 -0300

    add webdriver app

--- app/scrapers/headless_browser.rb
+++ app/scrapers/headless_browser.rb
@@ -8,11 +8,11 @@ class HeadlessBrowser
     def self.initialize_browser(url)
         chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
 
+        chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
         Capybara.register_driver :selenium do |app|  
             options = ::Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu window-size=1400,900])
-            options.binary = chrome_bin
-
-            Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+            Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts))
         end
         Capybara.configure do |config|  
             config.default_max_wait_time = 10 # seconds
