a614f64e5e0db42d1a58737d491392eee05edbf2
Bruno Tostes <btfjulio@hotmail.com>
2019-12-06 18:24:48 -0300

    create centauro scraper

--- app/scrapers/headless_browser.rb
+++ app/scrapers/headless_browser.rb
@@ -11,11 +11,13 @@ class HeadlessBrowser
         chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
         user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'
         Capybara.register_driver :selenium do |app|  
-            options = ::Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox disable-gpu window-size=1400,900 ignore-ssl-errors=yes load-images=no ssl-protocol=any] << "--user-agent='#{user_agent}'")
+            options = ::Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox disable-gpu headless window-size=1400,900 ignore-ssl-errors=yes load-images=no ssl-protocol=any] << "--user-agent='#{user_agent}'")
             Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts))
         end
         Capybara.javascript_driver = :headless_chrome
-        Capybara.configure do |config|  
+        Capybara.configure do |config| 
+            config.run_server = false
+            config.app_host   = 'http://www.google.com' 
             config.default_max_wait_time = 10 # seconds
             config.default_driver = :selenium
         end
@@ -23,6 +25,7 @@ class HeadlessBrowser
         browser = Capybara.current_session
         driver = browser.driver.browser
         browser.visit url
+        driver.execute_script('window.scrollTo(0, 0);')
         sleep(2) 
         doc = browser.body
         # rowser.driver.browser.close
