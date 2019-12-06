c1d61946b91871ad7839fce3da2d6371236c322b
Bruno Tostes <btfjulio@hotmail.com>
2019-12-06 20:20:16 -0300

    implement new headers to browser

--- app/scrapers/headless_browser.rb
+++ app/scrapers/headless_browser.rb
@@ -11,7 +11,7 @@ class HeadlessBrowser
         chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
         user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'
         Capybara.register_driver :selenium do |app|  
-            options = ::Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox disable-gpu headless window-size=1400,900 ignore-ssl-errors=yes load-images=no ssl-protocol=any] << "--user-agent='#{user_agent}'")
+            options = ::Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox disable-gpu headless window-size=1400,900 referrer=https://google.com accept-language=en-US,en;q=0.9 ignore-ssl-errors=yes load-images=no ssl-protocol=any] << "--user-agent='#{user_agent}'")
             Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts))
         end
         Capybara.javascript_driver = :headless_chrome
