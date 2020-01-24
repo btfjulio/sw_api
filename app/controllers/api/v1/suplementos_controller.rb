1b7f94a3eb2c29e043d52ce4e689cf9ba60cf869
Bruno Tostes <btfjulio@hotmail.com>
2020-01-24 16:29:16 -0300

    add coupons api endpoint

--- app/controllers/api/v1/suplementos_controller.rb
+++ app/controllers/api/v1/suplementos_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::SuplementosController < Api::V1::BaseController
       @suplementos = Suplemento.select('*, ((price_cents - average) / (average / 100)) as diff').where('price_cents < average').order('suplementos.diff') if params[:average].present?
       @suplementos = @suplementos.order(:price_cents) unless params[:average].present?
       @suplementos = @suplementos.where(price_changed: true) if params[:changed].present?
+      @suplementos = @suplementos.where(store_id: params[:store]) if params[:store].present?
       @suplementos = @suplementos.where.not(promo: '') if params[:promo].present?
       @suplementos = @suplementos.where(supershipping: true) if params[:supershipping].present?
-      @suplementos = @suplementos.store_search(params[:store]) if params[:store].present?
       @suplementos = @suplementos.seller_search(params[:seller]) if params[:seller].present?
       @suplementos = @suplementos.name_search(params[:name]) if params[:name].present?
       total_pages = (@suplementos.size.to_f / 50).ceil
