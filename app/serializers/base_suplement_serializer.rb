1b7b68fc8bc7e8c45bcb7d69ddc9cab5b3de24fe
Bruno Tostes <btfjulio@hotmail.com>
2020-04-02 22:56:28 -0300

    adjust api

--- app/serializers/base_suplement_serializer.rb
+++ app/serializers/base_suplement_serializer.rb
@@ -4,9 +4,17 @@ class BaseSuplementSerializer < ActiveModel::Serializer
   #  has_many :sup_photos
   def attributes(*args)
     base_suplement = super(*args)
-    base_suplement[:name] = object.name[0..((object.name =~ /\(/) - 1)].strip.titleize
+    base_suplement[:name] = serialize_name(object.name)
     base_suplement[:product_photos_attributes] = exclude_fields(object.sup_photos)
     base_suplement
+  end
+
+  def serialize_name(product_name)
+    index = product_name =~ /\(/
+    if index
+      product_name = product_name[0..((product_name =~ /\(/) - 1)]
+    end
+    product_name.strip.titleize
   end
 
   def exclude_fields(photos)
