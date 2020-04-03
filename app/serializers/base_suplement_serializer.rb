2edbb86feedb05de6c6b92e51607fec15a618cf5
Bruno Tostes <btfjulio@hotmail.com>
2020-04-02 22:06:32 -0300

    insert base suplement id on api

--- app/serializers/base_suplement_serializer.rb
+++ app/serializers/base_suplement_serializer.rb
@@ -1,5 +1,5 @@
 class BaseSuplementSerializer < ActiveModel::Serializer
-  attributes :name, :category, :subcategory, :flavor, :ean, :weight
+  attributes :id, :name, :category, :subcategory, :flavor, :ean, :weight
   belongs_to :brand
   #  has_many :sup_photos
   def attributes(*args)
