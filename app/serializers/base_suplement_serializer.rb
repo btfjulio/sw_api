9069e9c197b331ca6a25c82f79c3e69cb8de474b
Bruno Tostes <btfjulio@hotmail.com>
2020-04-02 21:49:32 -0300

    filter photo hashs

--- app/serializers/base_suplement_serializer.rb
+++ app/serializers/base_suplement_serializer.rb
@@ -1,11 +1,16 @@
 class BaseSuplementSerializer < ActiveModel::Serializer
   attributes :name, :category, :subcategory, :flavor, :ean, :weight
   belongs_to :brand
-  has_many :sup_photos
+  #  has_many :sup_photos
   def attributes(*args)
     base_suplement = super(*args)
     base_suplement[:name] = object.name[0..((object.name =~ /\(/) - 1)].strip.titleize
+    base_suplement[:product_photos_attributes] = exclude_fields(object.sup_photos)
     base_suplement
+  end
+
+  def exclude_fields(photos)
+    photos.map { |photo| photo.slice(:name, :size, :url) }
   end
 
 end
