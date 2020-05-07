8caca6c67a96dbf7217ad5384c80b5302181195a
Bruno Tostes <btfjulio@hotmail.com>
2020-05-07 20:28:07 -0300

    add search to equip

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,12 +1,15 @@
 class EquipmentsController < SuplementosController
     layout 'suplementos_layout'
-    # before_action :get_stores, only: [:index]
-    # before_action :get_filters, only: [:index]
-    # before_action :get_sellers, only: [:index]
+    before_action :get_equip_filters, only: [:index]
+    before_action :get_equip_sellers, only: [:index]
 
 
     def index 
       @equipments = Equipment.all
+      apply_filters(params[:filters])
+      @equipments = @equipments.where(store_id: params[:store]) if params[:store].present?
+      @equipments = @equipments.seller_search(params[:seller]) if params[:seller].present?
+      @equipments = @equipments.name_search(params[:name]) if params[:name].present?
       @equipments = @equipments.page(params[:page]).per(28)
     end
 
@@ -15,6 +18,26 @@ class EquipmentsController < SuplementosController
         @equipment = params[:equipment]
         @link = params[:link]
         @link = client.shorten(@link).short_url
+    end
+
+    def get_equip_sellers
+      @equip_sellers = Equipment.order(:seller).pluck(:seller).uniq
+    end
+
+    def get_equip_filters
+        @equip_filters = ['combo', 'frete', 'cupom', 'preço']
+    end
+  
+    def apply_filters(filters)
+        if filters
+            @equipments = @equipments.order(price: :asc) if (filters.include?("price") || !filters.include?("average"))
+            @equipments = @equipments.where('price_cents < average').order('discount') if filters.include?("average")
+            @equipments = @equipments.where('promo IS NOT NULL') if filters.include?("cupom")
+            @equipments = @equipments.where(free_shipping: true) if filters.include?("frete")
+            @equipments = @equipments.where(combo: "true") if filters.include?("combo")
+        else
+            @equipments = @equipments.order(price: :asc)
+        end    
     end
     
 end
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,3 +1,15 @@
 class Equipment < ApplicationRecord
     belongs_to :store
+
+    include PgSearch::Model
+    pg_search_scope :seller_search,  
+    against: :seller,
+    using: {
+        tsearch: { prefix: true }
+    }
+    pg_search_scope :name_search,
+    against: :name,
+    using: {
+        tsearch: { prefix: true }
+    }
 end
