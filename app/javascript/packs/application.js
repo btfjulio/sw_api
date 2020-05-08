01bfb7b6bdfb98a1c8e8d0583a18c53bf326a66f
Bruno Tostes <btfjulio@hotmail.com>
2020-05-08 11:38:28 -0300

    search style finished

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,25 +2,26 @@ import "bootstrap";
 import $ from "jquery";
 import "select2/dist/css/select2.css";
 import "select2";
-import 'animate.css';
+import "animate.css";
 
 const refreshStatus = () => {
   let list = document.querySelectorAll(".saudi_choice");
-  list.forEach(dropdown => {
-    dropdown.addEventListener("change", e => {
+  list.forEach((dropdown) => {
+    dropdown.addEventListener("change", (e) => {
       e.currentTarget.closest("form").submit();
     });
   });
 };
 
-
-const initSelect2 = () => {
-  $(".select2").select2({
-    placeholder: "Select filters",
-    width: '160px'
+const initSelect2 = (tag) => {
+  $(tag).select2({
+    placeholder: "Selecione os filtros..",
+    width: "160px",
   }); // (~ document.querySelectorAll)
 };
 
-initSelect2();
+document.querySelectorAll('.select2').forEach(initSelect2)
+
+
 
 refreshStatus();
