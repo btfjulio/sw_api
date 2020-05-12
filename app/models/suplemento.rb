9f3b1f3be4462b953d7772a0d21dd5c7627ac430
Bruno Tostes <btfjulio@hotmail.com>
2020-05-12 10:45:57 -0300

    change delete prices method

--- app/models/suplemento.rb
+++ app/models/suplemento.rb
@@ -28,7 +28,7 @@ class Suplemento < ApplicationRecord
   def delete_old_prices(query)
     prices = self.prices.order(:created_at)
     while prices.count > 30
-        prices.limit(size - 30).delete_all
+        prices.limit(prices.size - 30).delete_all
         puts "old prices deleted for #{self.name}"
     end
   end
