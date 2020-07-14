684a29a9e4568c82ab346a45225260de0f1f707c
Bruno Tostes <btfjulio@hotmail.com>
2020-07-14 15:30:31 -0300

    add seller id to netshoes link

--- app/scrapers/saudi_product_scraper.rb
+++ app/scrapers/saudi_product_scraper.rb
@@ -99,8 +99,11 @@ class SaudiProductScraper
 
   def parse_script(target_script)
     json_string = target_script.first.children.text.match(/PaginaInfo = (?<product_info>.+);var/)
-    return nil if json_string.nil?
-    parsed_json = JSON.parse( json_string, { symbolize_names: true })
+    begin
+      JSON.parse(json_string, { symbolize_names: true })
+    rescue => exception
+      return nil
+    end
   end
 
   def create_headers
