f18dc56dc13c0d2cb9323baeaabeb20ffbab7362
Bruno Tostes <btfjulio@hotmail.com>
2020-12-15 11:07:15 -0300

    change brands search name

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,18 +2,8 @@ class Brand < ApplicationRecord
   has_many :base_suplements
   include PgSearch::Model
   pg_search_scope :search_name, 
-  against: [:search_name],
-  using: {
-    tsearch: { prefix: true }
-  }
-
-  pg_search_scope :search_name_trigram,
                 against: [:search_name],
                 using: {
-                  trigram: {
-                    threshold: 0.3
-                  }
-                }
-
-                
+                  tsearch: { prefix: true }
+                }   
 end
