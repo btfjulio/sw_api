80b30bbdb8fbf5f377789c8c701a5547c041b264
Bruno Tostes <btfjulio@hotmail.com>
2020-12-17 12:15:57 -0300

    remove pry from dbsavingservice

--- app/services/db_saving_service.rb
+++ app/services/db_saving_service.rb
@@ -7,7 +7,7 @@ class DbSavingService
       sup = Suplemento.find_by(store_code: @product[:store_code]) 
       sup.present? ? sup.update(@product) : Suplemento.create(@product) 
     rescue StandardError => e
-      binding.pry
+      puts e
   end
 
 end
